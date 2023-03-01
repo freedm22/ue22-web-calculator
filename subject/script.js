46353543336c8da7dbb00b9145c124fa1d0a2686
freedm <freedm@orange.fr>
2023-03-01 16:50:29 +0100

    debut

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,42 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+document.addEventListener("DOMContentLoaded", () => 
+{   
+    let s = 0 ;
+    let current_num = "" ;
+
+    for (let num of document.getElementsByClassName("number")) 
+    {
+        num.addEventListener("click",(event)=>
+        {
+            document.getElementById("display").textContent = document.getElementById("display").textContent + event.target.textContent;
+            current_num = current_cum + event.target.textContent
+        }
+        )
+    };
+
+    document.getElementById("clear").addEventListener("click",()=>
+        {
+            document.getElementById("display").textContent = ""
+        }
+    );
+
+    for (let ope of document.getElementsByClassName("key--operator")) 
+    {
+        ope.addEventListener("click",(event)=>
+        {
+            document.getElementById("display").textContent = document.getElementById("display").textContent + event.target.textContent ;
+        }
+        )
+    };
+
+    document.getElementById("equal").addEventListener("click",()=>
+    {
+        document.getElementById("display").textContent = document.getElementById("display").textContent + "=" + s
+    }
+);
+
+}
+);
